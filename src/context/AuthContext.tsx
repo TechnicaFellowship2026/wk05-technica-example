cb70adfd47e54b8b89c62a12203afd2b121c52b6
Gavin <gcrisolo@terpmail.umd.edu>
2026-04-07 17:22:02 -0400

    updated AuthContext.tsx comments

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,17 +3,17 @@ import { onAuthStateChanged, type User } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import type { AuthContextType } from "../types/types";
 
-// 1. Create the Context
+// Create context
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-// 2. Create the Custom Hook (Consumers use this)
+// Used by components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error("useAuth must be used within AuthProvider");
   return context;
 };
 
-// 3. Create the Provider Component (Wraps the app)
+// Provider wraps the entire app
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
@@ -36,38 +36,3 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-// import { useEffect, useState } from "react";
-// import { onAuthStateChanged } from "firebase/auth";
-// import type { User } from "firebase/auth";
-// import { auth } from "../firebase/firebase";
-// import { AuthContext } from "./auth-context";
-
-// export const AuthContext = ({ children }: { children: React.ReactNode }) => {
-//   const [currentUser, setCurrentUser] = useState<User | null>(null);
-//   const [loading, setLoading] = useState(true);
-
-//   // Same stuff as the old example
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       setCurrentUser(user);
-//       setLoading(false);
-//     });
-
-//     // return unsubscribe f'n to be called on component unmount
-//     return unsubscribe;
-//   }, []);
-
-//   // We're putting in an object here the info we want to pass on
-//   const value = { currentUser, loading };
-
-//   return (
-//     <AuthContext.Provider value={value}>
-//       {/* children of this component in the DOM will have access to 
-//       AuthContext via useAuth
-
-//       Only returns children if finished loading
-//       */}
-//       {!loading && children}
-//     </AuthContext.Provider>
-//   );
-// };
